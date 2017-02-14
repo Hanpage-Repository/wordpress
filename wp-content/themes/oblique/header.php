7ce95e20d3807c3d07d11c34139501b9183033e9
root <root@ip-172-31-1-244.ap-northeast-2.compute.internal>
2017-02-14 04:57:27 +0000

    google code update

--- wp-content/themes/oblique/header.php
+++ wp-content/themes/oblique/header.php
@@ -22,18 +22,15 @@
 <meta property="og:url" content="http://hanpage.net">
 <meta property="og:image" content="http://d1fav6wwj1iaz1.cloudfront.net/public/016.png">
 
-
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
- (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
- m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ 	 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ 	 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ 	 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-91873110-1', 'auto');
-ga('send', 'pageview');
-
+	ga('create', 'UA-69982038-1', 'auto');
+	ga('send', 'pageview');
 </script>
-
 
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
