0b03a3e81de09a99b750e57e3875fd82a0940d14
root <root@ip-172-31-1-244.ap-northeast-2.compute.internal>
2017-05-26 06:46:23 +0000

    seo

--- wp-content/themes/oblique/header.php
+++ wp-content/themes/oblique/header.php
@@ -15,12 +15,14 @@
 
 <!-- added by KH  -->
 <meta name="naver-site-verification" content="b676af98f9416dcfe882719998d94a34ba22dbcc"/>
+<!-- edit by KH
 <meta name="description" content="개인용 프로필, 기업 홈페이지, 동호회·동아리·이벤트 전용 페이지 제작 등 서비스 제공.">
 <meta property="og:type" content="website">
 <meta property="og:title" content="한 페이지">
 <meta property="og:description" content="개인용 프로필, 기업 홈페이지, 동호회·동아리·이벤트 전용 페이지 제작 등 서비스 제공.">
 <meta property="og:url" content="http://hanpage.net">
 <meta property="og:image" content="http://d1fav6wwj1iaz1.cloudfront.net/public/016.png">
+-->
 
 <script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
